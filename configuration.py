491aa8481bea617699c12abca58e43f15cd759b1
Rost13 <rostpo13@gmail.com>
2024-01-23 15:30:06 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+
+
+URL_SERVICE = "https://2fed319c-ff55-4ba4-9767-ac1afb80122a.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ data.py
@@ -0,0 +1,7 @@
+
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {"firstName": "Aa","phone": "+74441237887","address": "г. Москва, ул. Хохотушкина, д. 16"}
